01bab40722cee3b86c16169028c0eabeddc41827
Himanshu Dimri <himanshudimrifullstack@gmail.com>
2026-02-14 11:51:47 +0530

    Add routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import Footer from './components/footer/footer'
 import Navbar from './components/navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
+import Products from './pages/Products'
 
 function App() {
 
   return (
     <div className="">
       <Navbar />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
       <Footer />
     </div>
   )
--- src/components/navbar/NavbarBanner.jsx
+++ src/components/navbar/NavbarBanner.jsx
@@ -1,49 +1,38 @@
 import React from 'react'
 import { Menu } from "lucide-react";
+import { Link } from 'react-router-dom';
 
 const NavbarBanner = () => {
+    const options = [
+        { "name": "Mx Player" },
+        { "name": "Sell" },
+        { "name": "Bestsellers" },
+        { "name": "Today's Deal" },
+        { "name": "Mobiles" },
+        { "name": "Customer service" },
+        { "name": "New Releases" },
+        { "name": "Fashion" },
+        { "name": "Electronics" },
+        { "name": "Amazon Pay" },
+        { "name": "Home & kitchen" },
+        { "name": "Computers" },
+    ]
     return (
         <div className='flex justify-around bg-[#232f3e] h-10'>
             <div className='flex pt-2 border border-transparent hover:border hover:border-white'>
                 <Menu className='text-white' size={20} />
-                <p className='text-white px-1 font-bold text-sm'>All</p>
+                <div className='text-white px-1 font-bold text-sm'>All</div>
             </div>
-            <div className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
-                <a className='p-1' href='#'>MX Player</a>
-            </div>
-            <div className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
-                <a className='p-1' href='#'>Sell</a>
-            </div>
-            <div className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
-                <a className='p-1' href='#'>Bestsellers</a>
-            </div>
-            <div className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
-                <a className='p-1' href='#'>Today's Deals</a>
-            </div>
-            <div className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
-                <a className='p-1' href='#'>Mobiles</a>
-            </div>
-            <div className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
-                <a className='p-1' href='#'>Customer Service</a>
-            </div>
-            <div className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
-                <a className='p-1' href='#'>New Releases</a>
-            </div>
-            <div className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
-                <a className='p-1' href='#'>Fashion</a>
-            </div>
-            <div className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
-                <a className='p-1' href='#'>Electronics</a>
-            </div>
-            <div className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
-                <a className='p-1' href='#'>Amazon Pay</a>
-            </div>
-            <div className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
-                <a className='p-1' href='#'>Home & Kitchen</a>
-            </div>
-            <div className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
-                <a className='p-1' href='#'>Computers</a>
-            </div>
+            {
+                options.map((option, index) => {
+                    return (
+                        <Link to={'products'} className='text-white font-semibold pt-2 text-sm border border-transparent hover:border hover:border-white'>
+                            <div className='px-1'>{option.name}</div>
+                        </Link>
+
+                    )
+                })
+            }
         </div>
     )
 }
